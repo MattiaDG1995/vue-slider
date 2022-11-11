31b0bfd9ab7b794c44fcf1e54a76fd286b9d15b9
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-11 15:33:39 +0100

    end js

---
+++ main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        obj: [
+            {id:1, immagine:'./assets/img/imm1.jpg'},
+            {id:2, immagine:'./assets/img/imm2.jpg'},
+            {id:3, immagine:'./assets/img/imm3.jpg'},
+            {id:4, immagine:'./assets/img/imm4.jpg'},
+            {id:5, immagine:'./assets/img/imm5.jpg'}
+        ],
+        indice:0,
+    },
+
+    methods: {
+        slideRight: function(){
+            this.indice++;
+            if( this.indice > this.obj.length){
+                this.indice = 0;
+            }
+        },
+
+        slideLeft: function(){
+            this.indice--;
+            if( this.indice < -1){
+                this.indice = 4;
+            }
+        }
+
+    }
+})
